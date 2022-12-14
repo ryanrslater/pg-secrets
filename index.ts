372010d517f8a6759107cea4767318aaabf8d326
Ryan Slater <78631176+ryanrslater@users.noreply.github.com>
2022-12-14 11:31:43 +0000

    update

--- index.ts
+++ index.ts
@@ -1,31 +1,47 @@
 import pg from 'pg'
 
-import { SecretsManagerClient, GetSecretValueCommand, SecretsManagerClientConfig} from '@aws-sdk/client-secrets-manager'
+import { SecretsManagerClient, GetSecretValueCommand, SecretsManagerClientConfig, GetSecretValueCommandInput } from '@aws-sdk/client-secrets-manager'
 
 class PgSecrets {
-    private readonly client: SecretsManagerClient
+    private readonly secretClient: SecretsManagerClient
     private readonly secretArn: string | undefined
     private readonly region: string | undefined
     private readonly accessKeyId: string | undefined
     private readonly secretAccessKey: string | undefined
 
     constructor() {
+        if (!this.secretArn) throw new Error('Secret ARN not found')
+        if (!this.region) throw new Error('Region not found')
+        if (!this.accessKeyId) throw new Error('Access Key ID not found')
+        if (!this.secretAccessKey) throw new Error('Secret Access Key not found')
         const config: SecretsManagerClientConfig = {
             credentials: {
                 accessKeyId: this.accessKeyId,
-                secretAccessKey: this.secretAccessKey
-            }
+                secretAccessKey: this.secretAccessKey,
+            },
+            region: this.region
         }
-        this.client = new SecretsManagerClient({
-            region: this.region,
-
-        })
+        this.secretClient = new SecretsManagerClient(config)
         this.secretArn = process.env.SECRET_ARN
     }
 
-    private async getSecret(secretId: string) {
+    private async getSecret(): Promise<pg.ClientConfig> {
+        const config: GetSecretValueCommandInput = {
+            SecretId: undefined
+        }
+        const res = await this.secretClient.send(new GetSecretValueCommand(config))
+        if (!res.SecretString) throw new Error('Secret not found')
+        const data = JSON.parse(res.SecretString) as pg.ClientConfig
 
-        const { SecretString } = await this.client.send(new GetSecretValueCommand({ SecretId: secretId }))
+        return data
+
+    }
+    async client(): Promise<pg.Client> {
+        const data = await this.getSecret()
+        const client = new pg.Client(data)
+        await client.connect()
+        return client
     }
 }
+
 export default PgSecrets
